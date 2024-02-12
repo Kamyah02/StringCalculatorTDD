dd8e402d0187f4f795a2055c38ac099803b567ef
kamyah <kamyah@google.com>
2024-02-12 16:56:17 +0000

    Add test and initial implementation for custom delimiter

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -6,6 +6,21 @@ public class StringCalculator {
             return 0;
         }
 
+        // Check for custom delimiter
+        if (numbers.startsWith("//")) {
+            String delimiter = numbers.substring(2, 3);
+            numbers = numbers.substring(4);
+            // Allowing custom delimiter along with comma and new line
+            String[] numberArray = numbers.split("[," + delimiter + "\n]");
+            int sum = 0;
+
+            for (String num : numberArray) {
+                sum += Integer.parseInt(num);
+            }
+
+            return sum;
+        }
+
         // Allowing new line as well as comma as delimiters
         String[] numberArray = numbers.split("[,\n]");
         int sum = 0;
--- src/test/java/stringcalculator/TestStringCalculator.java
+++ src/test/java/stringcalculator/TestStringCalculator.java
@@ -28,4 +28,10 @@ public class TestStringCalculator {
         StringCalculator calculator = new StringCalculator();
         assertEquals(6, calculator.add("1\n2,3"));
     }
+
+    @Test
+    public void testCustomDelimiter() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(3, calculator.add("//;\n1;2"));
+    }
 }
